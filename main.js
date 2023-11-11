6723e405aa884787e9c9dbfd57149bd18fdcae5c
christianjjc <christianjjc@gmail.com>
2023-11-11 14:47:31 -0500

    [FT] abstraccion de funciones para mostrar/ocultar controles

--- main.js
+++ main.js
@@ -7,6 +7,29 @@ function showHide(mostrar, idNodo) {
   }
 }
 
+function cargarControles(cmbAusar, divArray, inicioArray, divMostrar) {
+  const cmb = document.getElementById(cmbAusar);
+  cmb.addEventListener("change", () => {
+    const valor = cmb.value;
+    switch (valor) {
+      case "0":
+        const combos = document.getElementsByClassName(divArray);
+        for (let i = inicioArray; i < combos.length; i++) {
+          showHide(false, combos[i].id);
+        }
+        break;
+      default:
+        showHide(true, divMostrar);
+        break;
+    }
+  });
+}
+
+cargarControles("cmbproductos", "divopciones", 1, "bancos");
+cargarControles("cmbbancos", "divopciones", 2, "metodospago");
+cargarControles("cmbmetodospago", "divopciones", 3, "monto_pagar");
+
+/* 
 const cmbproductos = document.getElementById("cmbproductos");
 cmbproductos.addEventListener("change", () => {
   const valor = cmbproductos.value;
@@ -54,3 +77,4 @@ cmbmetodospago.addEventListener("change", () => {
       break;
   }
 });
+ */
